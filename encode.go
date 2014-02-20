60e967f5a89e70757e120b9f20ee9100c7bb9338
Andrew Gallant <jamslam@gmail.com>
2014-02-20 00:15:31 -0500

    Remove outdated comment.

--- encode.go
+++ encode.go
@@ -1,18 +1,5 @@
 package toml
 
-// TODO: Build a decent encoder.
-// Interestingly, this isn't as trivial as recursing down the type of the
-// value given and outputting the corresponding TOML. In particular, multiple
-// TOML types (especially if tuples are added) can map to a single Go type, so
-// that the reverse correspondence isn't clear.
-//
-// One possible avenue is to choose a reasonable default (like structs map
-// to hashes), but allow the user to override with struct tags. But this seems
-// like a mess.
-//
-// The other possibility is to scrap an encoder altogether. After all, TOML
-// is a configuration file format, and not a data exchange format.
-
 import (
 	"bufio"
 	"errors"
