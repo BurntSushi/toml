ffaa107fbd880f6d18cd6fec9b511668dcad8639
Caleb Spare <cespare@gmail.com>
2016-07-07 16:28:37 -0700

    Fix incorrect error message
    This cleans up some wording that was misleading (reversed).

--- decode.go
+++ decode.go
@@ -505,5 +505,5 @@ func isUnifiable(rv reflect.Value) bool {
 }
 
 func badtype(expected string, data interface{}) error {
-	return e("cannot decode TOML %s into Go value of type %T", expected, data)
+	return e("cannot load TOML value of type %T into a Go %s", data, expected)
 }
