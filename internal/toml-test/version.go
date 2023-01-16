f7d4e0aeedeec36d57a2d67c89546064277f0f42
Martin Tournoij <martin@arp242.net>
2023-01-16 13:48:46 +0100

    Support unicode bare keys
    Like other TOML 1.1 features this is hidden behind a flag, and only
    supported for the tests.

--- internal/toml-test/version.go
+++ internal/toml-test/version.go
@@ -18,6 +18,7 @@ var versions = map[string]versionSpec{
 			"invalid/inline-table/linebreak-2",
 			"invalid/inline-table/linebreak-3",
 			"invalid/inline-table/linebreak-4",
+			"invalid/key/special-character", // Unicode can now be in bare keys.
 		},
 	},
 
@@ -27,36 +28,7 @@ var versions = map[string]versionSpec{
 			"valid/string/hex-escape", "invalid/string/bad-hex-esc", // \x..
 			"valid/datetime/no-seconds", // Times without seconds
 			"valid/inline-table/newline",
-		},
-	},
-
-	// Added in 1.0.0:
-	//   Leading zeroes in exponent parts of floats are permitted.
-	//   Allow raw tab characters in basic strings and multi-line basic strings.
-	//   Allow heterogenous values in arrays.
-	"0.5.0": versionSpec{
-		inherit: "1.0.0",
-		exclude: []string{
-			"valid/hetergeneous",
-			"valid/array/mixed-*",
-		},
-	},
-
-	// Added in 0.5.0:
-	//   Add dotted keys.
-	//   Add hex, octal, and binary integer formats.
-	//   Add special float values (inf, nan)
-	//   Add Local Date-Time.
-	//   Add Local Date.
-	//   Add Local Time.
-	//   Allow space (instead of T) to separate date and time in Date-Time.
-	//   Allow accidental whitespace between backslash and newline in the line
-	//   continuation operator in multi-line basic strings.
-	"0.4.0": versionSpec{
-		inherit: "0.5.0",
-		exclude: []string{
-			"valid/datetime/local*",
-			"valid/key/dotted",
+			"valid/key/unicode", // Unicode bare keys
 		},
 	},
 }
